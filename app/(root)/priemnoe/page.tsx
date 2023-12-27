88dcac8f5cf38bd1a5b6122a8a2609279cc12316
Artem <68074370+SedifBuster@users.noreply.github.com>
2023-12-27 10:02:33 +0300

    add

--- app/(root)/priemnoe/page.tsx
+++ app/(root)/priemnoe/page.tsx
@@ -1,10 +1,11 @@
+import { Admin } from "../(lk)/lk/components/roles/Admin";
 import { DashPriemnoe } from "./components/DashPriemnoe";
 
 
 export default function Priemnoe() {
     return (
       <div className="flex justify-center">
-                <DashPriemnoe />
+            <Admin />
       </div>
     )
   }
