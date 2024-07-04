a58eea11672a4d5494542d3b7b9819ef8a3fd9cf
Artem <68074370+SedifBuster@users.noreply.github.com>
2024-07-04 16:51:46 +0300

    work dash

--- app/dashboard/components/DashItem.tsx
+++ app/dashboard/components/DashItem.tsx
@@ -475,16 +475,92 @@ export
     return dashDepsWithId
   }
 
-  let onSortDepartmentWards = (wards: DashWard[]) => {
+  let onSortDepartmentWards = (wards: any) => {
+    //let filtredArr = wards.map((ward) => {
+     // return 
+    //})
+    
+    //let filteredArr = wards.filter((ward) => {
+    //  return ward.number === 
+    //})
 
-    wards.filter((ward) => {
-      return 
-    })
+    /**let uniqSet = new Set()
+    //let testSet = new Set()
+    //let uniqSet = []
+    let testSet = []
     for(let i = 0; i < wards.length; i++) {
+      for(let j = 0; j < i; j++) {
+        if(wards[j].number === wards[i].number) {
+          uniqSet.add(wards[i])
+          //uniqSet.push(wards[i])
+        }// else {
+          //testSet.add(wards[j])
+          //testSet.push(wards[j])
+        //}
+      }
+    }
 
-      wards[i].number === 
+    let uniq = Array.from(uniqSet)//uniqSet//
+   // let test = testSet//Array.from(testSet)
+    //console.log(uniq)
+
+    return [...uniq, [...wards]]*/
+
+    let res: DashWard[] = []
+    console.log(res)
+    for(let i = 0; i < wards.length; i++) {
+     let duplicate = wards.filter((w: any) => w.number === wards[i].number)
+
+     if(duplicate)
+      if(duplicate.length === 1){
+        res.push(wards[i])
+      } else if(duplicate.length > 1){
+
+        wards.splice(i, 1)
+
+        res = [...res, duplicate]
+
+        let onS = wards.findIndex((w: any) => w.number === wards[i].number)
+        console.log(onS)
+        if(onS)
+          wards.splice(onS, 1)
+         //res = [...res, duplicate]
+
+        //res = [...res]
+
+      } else res = [...res]
 
     }
+
+    return res
+  /*let sorted = wards.slice().sort((a, b) =>  a.number - b.number)
+  console.log(sorted)
+  let results = [];
+  for (let i = 1; i < sorted.length; i++) {
+    if (sorted[i - 1].number === sorted[i].number) {
+      results.push(sorted[i]);
+    }
+
+  }
+  let testRes = []
+  if(results)
+  for (let j = 1; j < results.length; j++) {
+    if (results[j - 1].number === results[j].number) {
+      testRes.push(results[j]);
+    }
+
+  }
+  console.log(testRes)
+  let test = Array.from(new Set(results))*/
+
+  //return [...test]
+
+  //const findUniqueDuplicates = (arr: DashWard[]) => arr.filter((e, i, a) => a.indexOf(e) !== i && a.lastIndexOf(e) === i);
+
+  //return findUniqueDuplicates(wards)
+
+
+
   }
 
   let getWardsDeparment = async () => {
@@ -529,14 +605,17 @@ export
        }).filter((ward: any) => {
         return ward.status !== 'deleted' && ward.status !== 'disabled'
        })
-       console.log(withoutAfterWards)
+       //console.log(withoutAfterWards)
        //@ts-ignore
        dashDepsWithId[i].wards = withoutAfterWards
+       //@ts-ignore
+      // dashDepsWithId[i].wards = onSortDepartmentWards(dashDepsWithId[i].wards)
        //отсев левых
        //сортировка по дате
        //полученные
        //отданные
        //удаленные нахуй
+       console.log(onSortDepartmentWards(dashDepsWithId[i].wards))
        //В КОНЦЕ ДОЛЖНО БЫТЬ
       //@ts-ignore
       dashDepsWithId[i].totalStays = dashDepsWithId[i].wards.reduce((acc, currentValue) => acc + currentValue.engaged, 0)
@@ -555,9 +634,6 @@ export
       toast.error(`ошибка при получении палат: ${(error as Error).message}`)
     }
   }
-
-  //console.log(isWardsNow)
-  //console.log(isData)
 
   useEffect(() => {
     getWardsDeparment()
