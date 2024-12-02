c7358754dc90b89ea312a1e669996033f02cce27
Artem <68074370+SedifBuster@users.noreply.github.com>
2024-12-02 16:14:16 +0300

    fixes subcategories

--- app/(root)/(bdKnowlegde)/bdKnowledge/components/FilesContainer.tsx
+++ app/(root)/(bdKnowlegde)/bdKnowledge/components/FilesContainer.tsx
@@ -1,11 +1,26 @@
 "use client"
 
+import { Button } from "@/components/ui/button"
+import { DropdownMenu,
+   DropdownMenuContent,
+    DropdownMenuGroup,
+     DropdownMenuItem,
+      DropdownMenuLabel,
+       DropdownMenuPortal,
+        DropdownMenuSeparator,
+         DropdownMenuShortcut,
+          DropdownMenuSub,
+           DropdownMenuSubContent,
+            DropdownMenuSubTrigger,
+             DropdownMenuTrigger
+             } from "@/components/ui/dropdown-menu"
 import { Label } from "@/components/ui/label"
 import { FileBd, SubFileCategory } from "@prisma/client"
+
 import axios from "axios"
 import Link from "next/link"
 import { useEffect, useState } from "react"
-import { HiOutlineDocument } from "react-icons/hi2"
+import { HiOutlineArrowUturnDown, HiOutlineDocument } from "react-icons/hi2"
 
 export
   function FilesContainer({
@@ -70,18 +85,40 @@ export
                         
                           <div key={keysub} className="px-2">
                             { //@ts-ignore
-                              keysub !== "null" ? <h6>{keysub}</h6>
-                              :
-                              null
-                            }
+                              keysub !== "null" ? 
+                              <DropdownMenu>
+                              <DropdownMenuTrigger asChild>
+                              <h6 className="cursor-pointer flex items-center gap-1">{keysub}<HiOutlineArrowUturnDown /></h6>
+                              </DropdownMenuTrigger>
+                              <DropdownMenuContent className="w-56">
+                                <DropdownMenuLabel>{keysub}</DropdownMenuLabel>
+                                <DropdownMenuSeparator />
+                                <DropdownMenuGroup>
+                                   {//@ts-ignore
+                                  groupBy(finalArray[key], "subCategory")[keysub].map((key: FileBd) => {
+                                    return <DropdownMenuItem key={key.id}>
+                              <Link href={`http://192.168.0.148:5000/knowledgeBd/${key.filePath}`} target="_blank" className="flex gap-2 items-center">
+                                <HiOutlineDocument />
+                                <p className="text-sm text-blue-700">{key.name}</p>
+                              </Link>
+                            </DropdownMenuItem>
+                          })
+                        }
+                       </DropdownMenuGroup>
+                     </DropdownMenuContent>
+                  </DropdownMenu>
+                  :
+                  null
+                }
+
                       <ul className="flex flex-col justify-center p-2 gap-2">
                         {//@ts-ignore
-                        
                           groupBy(finalArray[key], "subCategory")[keysub].map((key: FileBd) => {
+                            if(keysub === "null")
                             return <li key={key.id}>
                               <Link href={`http://192.168.0.148:5000/knowledgeBd/${key.filePath}`} target="_blank" className="flex gap-2 items-center">
                                 <HiOutlineDocument />
-                                <p className="text-sm text-blue-700">{key.subCategory} {key.name}</p>
+                                <p className="text-sm text-blue-700">{key.name}</p>
                               </Link>
                             </li>
                           })
