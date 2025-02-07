c4dffc56d796ce578511fe95aad882dba5e12251
Artem <68074370+SedifBuster@users.noreply.github.com>
2025-02-07 16:46:39 +1000

    todo

--- app/zhus/page.tsx
+++ app/zhus/page.tsx
@@ -9,6 +9,9 @@ export default function Zhus() {
     //2. сделать выборку с датой COMPLETE
     //3. победить саброу COMPLETE
     //3.1 3.2  ссл и pnpm и nginx
+    //4. фильтр по отделениям
+    //5. таблица уходит влево
+    //6. возможность коммент запилить
     return (
       <main className="flex justify-center">
         <div>
