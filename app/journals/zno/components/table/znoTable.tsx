8f72c0bbb1adf1ecb5edfa6c8e5cb68856e412c0
Artem <68074370+SedifBuster@users.noreply.github.com>
2025-04-28 11:41:51 +1000

    added polya

--- app/journals/zno/components/table/znoTable.tsx
+++ app/journals/zno/components/table/znoTable.tsx
@@ -4,6 +4,7 @@ import * as React from "react"
 import {
   ColumnDef,
   ColumnFiltersState,
+  Row,
   SortingState,
   VisibilityState,
   getCoreRowModel,
@@ -521,6 +522,90 @@ export function ZnoTable({
   //локализация перевод и текущий статус перевод
 
   //функция переключения состояния и спрашивания какая у профиля должность
+  const [isName, setIsName] = React.useState<string>()
+
+  const onChangeChangedState = (row: Row<ZnoLog>) => {
+
+    //тут спрашиваем кто персонаж что меняет поле
+   
+
+
+  }
+
+
+
+
+  const onChangeRuLocalisations = (value: string) => {
+    switch (value) {
+      case 'ZNOOfTheLipOropharynx':
+        return "ЗНО ГУБЫ (МКБ-10: С00), РОТОГЛОТКИ (МКБ 10:С01, С02.4, C05.1, С05.2, C09, C10), ПОЛОСТИ РТА (МКБ 10:C02.0, C02.1, C02.2, C02.3, C02.8, C02.9, С03, С04, C05.0, С06)"
+      case 'MalignancyOfTheEsophagusCardia':
+        return "ЗНО ПИЩЕВОДА, КАРДИИ (МКБ-10: С15, С16.0)"
+      case 'ZNOStomachCancer':
+        return "ЗНО ЖЕЛУДКА  (МКБ-10: С16)"
+      case 'MalignancyOfTheColonRectoSigMoidJointRectumAnusAnal':
+        return "ЗНО ОБОДОЧНОЙ КИШКИ, РЕКТО-СИГМОИДНОГО СОЕДИНЕНИЯ, ПРЯМОЙ КИШКИ, ЗАДНЕГО ПРОХОДА (АНУСА) И АНАЛЬНОГО КАНАЛА  (МКБ-10: С18, С19, С20, С21)"
+      case 'CancerOfTheLiverAndIntraheraticBileDucts':
+        return "ЗНО ПЕЧЕНИ И ВНУТРИПЕЧЕНОЧНЫХ ЖЕЛЧНЫХ ПРОТОКОВ  (МКБ-10: С22)"
+      case 'OncologyOfTheBiliarySystem':
+        return "ЗНО ЖЕЛЧЕВЫВОДЯЩЕЙ СИСТЕМЫ (МКБ-10: С22.1, С23, С24.0)"
+      case 'PancreaticCancer':
+        return "ЗНО ПОДЖЕЛУДОЧНОЙ ЖЕЛЕЗЫ  (МКБ-10: С25)"
+      case 'NeuroendocrineTumor':
+        return "НЕЙРОЭНДОКРИНННОЙ ОПУХОЛИ (МКБ-10: C15, C16, C17, C18, C19.9, C20.9, C21, C23, C24, C25, C26, C34, C37.9, C73.9)"
+      case 'LarynxDisease':
+        return "ЗНО ГОРТАНИ  (МКБ-10: С32)"
+      case 'MalignancyOfTheTracheaLungMediastinumAndPleura':
+        return "ЗНО ТРАХЕИ, ЛЕГКОГО, ВИЛОЧКОВОЙ ЖЕЛЕЗЫ, СРЕДОСТЕНИЯ И ПЛЕВРЫ   (МКБ-10: С33, С34, С37, С38)"
+      case 'MelanomasAndMucousMembranes':
+        return "МЕЛАНОМЫ И СЛИЗИСТЫХ ОБОЛОЧЕК  (МКБ-10: C43, C51, C60.9, C63.2, C69.0, C00–C26, C30-C32, C52, C53 C77, C78, C79 D03.0-D03.9)"
+      case 'SquamousCellSkinCarcinomaBasalCellSkinCarcinomaMerkelsCarcinoma':
+        return "ПЛОСКОКЛЕТОЧНОГО РАКА КОЖИ, БАЗАЛЬНО-КЛЕТОЧНОГО РАКА КОЖИ, КАРЦИНОМЫ МЕРКЕЛЯ (МКБ-10: С44, D04)"
+      case 'SoftTissueSarcoma':
+        return "ЗНО САРКОМЫ МЯГКИХ ТКАНЕЙ  (МКБ-10: С 49)"
+      case 'BreastCancer':
+        return "ЗНО МОЛОЧНОЙ ЖЕЛЕЗЫ  (МКБ-10: С50)"
+      case 'MalignancyOfTheVaulvaVaginaCervix':
+        return "ЗНО ВУЛЬВЫ, ВЛАГАЛИЩА, ШЕЙКИ МАТКИ,  (МКБ-10: С51, С52, С53)"
+      case 'EndometralDisease':
+        return "ЗНО ЭНДОМЕТРИЯ  (МКБ-10: С54)"
+      case 'OvarianCancerBorderlineTumorsNonepithelialTumorsOvarianCancer':
+        return "ЗНО ЯИЧНИКОВ: ПОГРАНИЧНЫЕ ОПУХОЛИ, НЕЭПИТЕЛИАЛЬНЫЕ ОПУХОЛИ, РАК ЯИЧНИКОВ (РАК МАТОЧНОЙ ТРУБЫ), ПЕРВИЧНЫЙ РАК БРЮШИНЫ В ЦАОП (МКБ-10: С56, С48.0, С48.2, С57)"
+      case 'ProstateCancer':
+        return "ЗНО ПРЕДСТАТЕЛЬНОЙ ЖЕЛЕЗЫ  (МКБ-10: С61)"
+      case 'TesticularCancer':
+        return "ЗНО ЯИЧКА  (МКБ-10: С62)"
+      case 'MalignancyOfTheRenalPelvisGlasCase':
+        return "ЗНО ПАРЕНХИМЫ ПОЧКИ, ЛОХАНКИ, ОЧЕТОЧНИКА (МКБ-10: С 64, С65, С66 )"
+      case 'BladderCancer':
+        return "ЗНО МОЧЕВОГО ПУЗЫРЯ  (МКБ-10: С67)"
+      case 'PrimaryTumorsOfTheCentralNervousSystemLimphoma':
+        return "ПЕРВИЧНЫХ ОПУХОЛЕЙ ЦЕНТРАЛЬНОЙ НЕРВНОЙ СИСТЕМЫ (МКБ-10: С70, С71, С72, С75, С76), ПЕРВИЧНОЙ ЛИМФОМЫ ЦЕНТРАЛЬНОЙ НЕРВНОЙ СИСТЕМЫ (МКБ 10: С81, С82, С83, С84, С85, С86, С87, С88)"
+      case 'ThyroidCancer':
+        return "ЗНО ЩИТОВИДНОЙ ЖЕЛЕЗЫ (МКБ-10: С73)"
+      case 'ZnoWithoutPrimaryLocalization':
+        return "ЗНО БЕЗ ПЕРВИЧНОЙ ЛОКАЛИЗАЦИИ (МКБ-10: С80)"
+      case 'HodgkinsLymphomaInAdultsFollicularLymphomaInAdults':
+        return "ЛИМФОМА ХОДЖКИНА У ВЗРОСЛЫХ (МКБ-10: С81), ФОЛЛИКУЛЯРНАЯ ЛИМФОМА У ВЗРОСЛЫХ (МКБ-10: С82), НЕХОДЖКИНСКАЯ ЛИМФОМА (МКБ-10: С83)"
+      default:
+        return value
+    }
+  }
+
+  const onChangeRuStatus = (value: string) => {
+    switch (value) {
+      case 'awaitingReferralToCAOP':
+        return "ожидает направления в ЦАОП"
+      case 'awaitingReferralToPKOD':
+        return "ожидает направления в ПКОД"
+      case 'waitingForAConsultation':
+        return "Ожидает консилиум"
+      case 'Completed':
+        return "Завершено"
+      default:
+        return value
+    }
+  }
 
   const [isRefactoring, setIsRefactoring] = React.useState<boolean>(true)
 
@@ -539,7 +624,7 @@ export function ZnoTable({
           isChangedYellow && isRefactoring
           ?
           <div className="capitalize"> 
-          <Textarea value={row.getValue("name")} className="w-18 h-full"/>
+          <Textarea value={isName} className="w-18 h-full" onChange={(e) => setIsName(e.target.value)}/>
           </div>
           :
           <div className="capitalize"> {row.getValue("name")}  </div>
@@ -565,10 +650,21 @@ export function ZnoTable({
         isChangedYellow && isRefactoring
         ?
         <div className="capitalize">
-          {row.getValue("localization")}select
+           <Select onValueChange={row.getValue("localization")}  value={row.getValue("localization")} defaultValue={row.getValue("localization")}>
+              <SelectTrigger className="w-[320px]">
+                <SelectValue placeholder="не выбрано" />
+              </SelectTrigger>
+           <SelectContent>
+            {
+              localisations.map(dep => {
+                return <SelectItem key={dep.value} value={dep.value}>{dep.text}</SelectItem>
+            })
+            }
+           </SelectContent>
+           </Select>
           </div>
         :
-        <div className="capitalize">{row.getValue("localization")}</div>
+        <div className="capitalize">{onChangeRuLocalisations(row.getValue("localization"))}</div>
       )
     },
     {
@@ -576,7 +672,9 @@ export function ZnoTable({
       cell: ({ row }) => (
         isChangedYellow && isRefactoring
         ?
-        <div className="capitalize">{row.getValue("phoneNumber")}</div>
+        <div className="capitalize">
+          <Textarea value={row.getValue("phoneNumber")} className="w-18 h-full"/>
+        </div>
         :
         <div className="capitalize">{row.getValue("phoneNumber")}</div>
       )
@@ -649,7 +747,6 @@ export function ZnoTable({
                </div>
            </PopoverContent>
           </Popover>
-          {isDate(row.getValue("dateOfReferralToCAOP"))? format(row.getValue("dateOfReferralToCAOP"), "PPP HH:mm", {locale: ru}) : row.getValue("dateOfReferralToCAOP")}
           </div>
         :
         <div className="capitalize">{isDate(row.getValue("dateOfReferralToCAOP"))? format(row.getValue("dateOfReferralToCAOP"), "PPP HH:mm", {locale: ru}) : row.getValue("dateOfReferralToCAOP")}</div>
@@ -660,7 +757,34 @@ export function ZnoTable({
       cell: ({ row }) => (
         isChangedGreen && isRefactoring
         ?
-        <div className="capitalize">{isDate(row.getValue("dateOfVisitToCAOP"))? format(row.getValue("dateOfVisitToCAOP"), "PPP HH:mm", {locale: ru}) : row.getValue("dateOfVisitToCAOP")}</div>
+        <div className="capitalize">
+                    <Popover>
+            <PopoverTrigger asChild>
+              <Button
+                variant={"outline"}
+                className={cn(
+                  "w-[200px] justify-start text-left font-normal",
+                   !row.getValue("dateOfVisitToCAOP") && "text-muted-foreground"
+                )}
+              >
+                  <CalendarIcon className="mr-2 h-4 w-4" />
+                    {row.getValue("dateOfVisitToCAOP") ? format(row.getValue("dateOfVisitToCAOP"), "PPP", {locale: ru}) : <span>Выберите время*</span>}
+              </Button>
+             </PopoverTrigger>
+              <PopoverContent className="w-auto p-0" align="start">
+                <Calendar
+                 mode="single"
+                 selected={row.getValue("dateOfVisitToCAOP")}
+                 onSelect={row.getValue("dateOfVisitToCAOP")}
+                 initialFocus
+                 locale={ru}
+              />
+              <div className="p-2 flex justify-center border-t">
+               <DateTimePicker date={row.getValue("dateOfVisitToCAOP")} setDate={row.getValue("dateOfVisitToCAOP")}/>
+               </div>
+           </PopoverContent>
+          </Popover>
+           </div>
         :
         <div className="capitalize">{isDate(row.getValue("dateOfVisitToCAOP"))? format(row.getValue("dateOfVisitToCAOP"), "PPP HH:mm", {locale: ru}) : row.getValue("dateOfVisitToCAOP")}</div>
       )   
@@ -682,7 +806,34 @@ export function ZnoTable({
       cell: ({ row }) => (
         isChangedGreen && isRefactoring
         ?
-        <div className="capitalize">{isDate(row.getValue("dateOfVisitToPKOD"))? format(row.getValue("dateOfVisitToPKOD"), "PPP HH:mm", {locale: ru}) : row.getValue("dateOfVisitToPKOD")}</div>
+        <div className="capitalize">
+          <Popover>
+            <PopoverTrigger asChild>
+              <Button
+                variant={"outline"}
+                className={cn(
+                  "w-[200px] justify-start text-left font-normal",
+                   !row.getValue("dateOfVisitToPKOD") && "text-muted-foreground"
+                )}
+              >
+                  <CalendarIcon className="mr-2 h-4 w-4" />
+                    {row.getValue("dateOfVisitToPKOD") ? format(row.getValue("dateOfVisitToPKOD"), "PPP", {locale: ru}) : <span>Выберите время*</span>}
+              </Button>
+             </PopoverTrigger>
+              <PopoverContent className="w-auto p-0" align="start">
+                <Calendar
+                 mode="single"
+                 selected={row.getValue("dateOfVisitToPKOD")}
+                 onSelect={row.getValue("dateOfVisitToPKOD")}
+                 initialFocus
+                 locale={ru}
+              />
+              <div className="p-2 flex justify-center border-t">
+               <DateTimePicker date={row.getValue("dateOfVisitToPKOD")} setDate={row.getValue("dateOfVisitToPKOD")}/>
+               </div>
+           </PopoverContent>
+          </Popover>
+           </div>
         :
         <div className="capitalize">{isDate(row.getValue("dateOfVisitToPKOD"))? format(row.getValue("dateOfVisitToPKOD"), "PPP HH:mm", {locale: ru}) : row.getValue("dateOfVisitToPKOD")}</div>
       ) 
@@ -704,7 +855,34 @@ export function ZnoTable({
       cell: ({ row }) => (
         isChangedGreen && isRefactoring
         ?
-        <div className="capitalize">{isDate(row.getValue("dateOfTheConsultation"))? format(row.getValue("dateOfTheConsultation"), "PPP HH:mm", {locale: ru}) : row.getValue("dateOfTheConsultation")}</div>
+        <div className="capitalize">
+                    <Popover>
+            <PopoverTrigger asChild>
+              <Button
+                variant={"outline"}
+                className={cn(
+                  "w-[200px] justify-start text-left font-normal",
+                   !row.getValue("dateOfTheConsultation") && "text-muted-foreground"
+                )}
+              >
+                  <CalendarIcon className="mr-2 h-4 w-4" />
+                    {row.getValue("dateOfTheConsultation") ? format(row.getValue("dateOfTheConsultation"), "PPP", {locale: ru}) : <span>Выберите время*</span>}
+              </Button>
+             </PopoverTrigger>
+              <PopoverContent className="w-auto p-0" align="start">
+                <Calendar
+                 mode="single"
+                 selected={row.getValue("dateOfTheConsultation")}
+                 onSelect={row.getValue("dateOfTheConsultation")}
+                 initialFocus
+                 locale={ru}
+              />
+              <div className="p-2 flex justify-center border-t">
+               <DateTimePicker date={row.getValue("dateOfTheConsultation")} setDate={row.getValue("dateOfTheConsultation")}/>
+               </div>
+           </PopoverContent>
+          </Popover>
+          </div>
         :
         <div className="capitalize">{isDate(row.getValue("dateOfTheConsultation"))? format(row.getValue("dateOfTheConsultation"), "PPP HH:mm", {locale: ru}) : row.getValue("dateOfTheConsultation")}</div>
       ) 
@@ -714,7 +892,34 @@ export function ZnoTable({
       cell: ({ row }) => (
         isChangedGreen && isRefactoring
         ?
-        <div className="capitalize">{isDate(row.getValue("dateOfLastCallAndPersonalContact"))? format(row.getValue("dateOfLastCallAndPersonalContact"), "PPP HH:mm", {locale: ru}) : row.getValue("dateOfLastCallAndPersonalContact")}</div>
+        <div className="capitalize">
+          <Popover>
+            <PopoverTrigger asChild>
+              <Button
+                variant={"outline"}
+                className={cn(
+                  "w-[200px] justify-start text-left font-normal",
+                   !row.getValue("dateOfLastCallAndPersonalContact") && "text-muted-foreground"
+                )}
+              >
+                  <CalendarIcon className="mr-2 h-4 w-4" />
+                    {row.getValue("dateOfLastCallAndPersonalContact") ? format(row.getValue("dateOfLastCallAndPersonalContact"), "PPP", {locale: ru}) : <span>Выберите время*</span>}
+              </Button>
+             </PopoverTrigger>
+              <PopoverContent className="w-auto p-0" align="start">
+                <Calendar
+                 mode="single"
+                 selected={row.getValue("dateOfLastCallAndPersonalContact")}
+                 onSelect={row.getValue("dateOfLastCallAndPersonalContact")}
+                 initialFocus
+                 locale={ru}
+              />
+              <div className="p-2 flex justify-center border-t">
+               <DateTimePicker date={row.getValue("dateOfLastCallAndPersonalContact")} setDate={row.getValue("dateOfLastCallAndPersonalContact")}/>
+               </div>
+           </PopoverContent>
+          </Popover>
+          </div>
         :
         <div className="capitalize">{isDate(row.getValue("dateOfLastCallAndPersonalContact"))? format(row.getValue("dateOfLastCallAndPersonalContact"), "PPP HH:mm", {locale: ru}) : row.getValue("dateOfLastCallAndPersonalContact")}</div>
       )
@@ -725,10 +930,21 @@ export function ZnoTable({
         isChangedGreen && isRefactoring
         ?
         <div className="capitalize">
-          <Textarea value={row.getValue("status")} className="w-18 h-full"/>
+          <Select onValueChange={row.getValue("status")}  value={row.getValue("status")} defaultValue={row.getValue("status")}>
+            <SelectTrigger className="w-[320px]">
+              <SelectValue placeholder="не выбрано" />
+            </SelectTrigger>
+          <SelectContent>
+            {
+              statuses.map(dep => {
+                return <SelectItem key={dep.value} value={dep.value}>{dep.text}</SelectItem>
+            })
+            }
+           </SelectContent>
+           </Select>
           </div>
         :
-        <div className="capitalize">{row.getValue("status")}</div>
+        <div className="capitalize">{onChangeRuStatus(row.getValue("status"))}</div>
       )  
     },
     {
@@ -831,13 +1047,9 @@ export function ZnoTable({
         <Table>
           <ZnoTableHead/>
           <TableBody>
-            <>
-            
-            </>
             {table.getRowModel().rows?.length ? (
               table.getRowModel().rows.map((row) => (
                 <ZnoTableRow key={row.id} row={row}/>
-
               ))
             ) : (
               <TableRow>
